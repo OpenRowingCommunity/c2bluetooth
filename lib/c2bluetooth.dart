1a5abbb9264ccddc4ea99074b83761762ab85f98
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-12-09 10:27:47 -0500

    write a function to parse uint8's to an int

--- lib/c2bluetooth.dart
+++ lib/c2bluetooth.dart
@@ -13,8 +13,23 @@ class WorkoutSummary {
   final int distance;
 
   WorkoutSummary.fromBytes(Uint8List data)
-      : elapsedTime =
-            data.sublist(4, 7).buffer.asByteData().getUint64(0, Endian.little),
-        distance =
-            data.sublist(7, 10).buffer.asByteData().getUint64(0, Endian.little);
+      : elapsedTime = bytesToInt(data.sublist(4, 7), Endian.little),
+        distance = bytesToInt(data.sublist(7, 10), Endian.little);
+}
+
+/// takes in a sublist of bytes and converts it to an int
+int bytesToInt(Uint8List data, [Endian endian = Endian.big]) {
+  // if (data.length > 4) => return Error("Data may not be more than 4 bytes");
+
+  // make sure everything is formatted with the Most Significant Byte first.
+  if (endian != Endian.big) {
+    data = Uint8List.fromList(data.reversed.toList());
+  }
+
+  int result = 0;
+  for (final datapoint in data) {
+    result = result << 8;
+    result = result | datapoint;
+  }
+  return result;
 }
