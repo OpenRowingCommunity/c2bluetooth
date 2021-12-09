3aa64118f61c61c72d3ee57c70ee942b794b62f8
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-12-08 21:31:10 -0500

    add workoutSummary class to parse some of the workout summary data from bytes

--- lib/c2bluetooth.dart
+++ lib/c2bluetooth.dart
@@ -5,3 +5,9 @@ class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
+class WorkoutSummary {
+  final int elapsedTime;
+
+  WorkoutSummary.fromBytes(Uint8List data)
+      : elapsedTime = data.sublist(4, 7).buffer.asByteData().getUint64(0);
+}
