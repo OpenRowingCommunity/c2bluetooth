21d7aec711d69b27e79198af994d19bcd7a8f83c
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-12-09 16:06:45 -0500

    first pass at making some classes to wrap around BleManager and Peripheral

--- lib/c2bluetooth.dart
+++ lib/c2bluetooth.dart
@@ -1,3 +1,54 @@
 library c2bluetooth;
 
 import 'models/workoutsummary.dart';
+import 'package:c2bluetooth/constants.dart' as Identifiers;
+import 'package:flutter_ble_lib/flutter_ble_lib.dart';
+
+class ErgBleManager {
+  BleManager manager = BleManager();
+
+  // ErgBleManager() {}
+
+  void init() {
+    manager.createClient();
+  }
+
+  Stream<Ergometer> startErgScan() {
+    // TODO: filter by UUID
+    return manager
+        .startPeripheralScan()
+        .map((scanResult) => Ergometer.fromPeripheral(scanResult.peripheral));
+  }
+
+  Future<void> stopErgScan() {
+    return manager.stopPeripheralScan();
+  }
+}
+
+class Ergometer {
+  Peripheral? peripheral;
+
+  Ergometer.fromPeripheral(Peripheral peripheral) {
+    this.peripheral = peripheral;
+  }
+
+  Future<void> connectAndDiscover() async {
+    if (peripheral == null) return;
+
+    await peripheral!.connect();
+    await peripheral!.discoverAllServicesAndCharacteristics();
+  }
+
+  Future<void> disconnectOrCancel() async {
+    if (peripheral == null) return;
+
+    return peripheral?.disconnectOrCancelConnection();
+  }
+
+  Stream<WorkoutSummary> monitorForWorkoutSummary() {
+    return peripheral!
+        .monitorCharacteristic(Identifiers.C2_ROWING_PRIMARY_SERVICE_UUID,
+            Identifiers.C2_ROWING_END_OF_WORKOUT_SUMMARY_CHARACTERISTIC_UUID)
+        .asyncMap((datapoint) => datapoint.read().then((dp) => WorkoutSummary.fromBytes(dp)));
+  }
+}
