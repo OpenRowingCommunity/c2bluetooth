6962652d4e0f353b4dc554dee250de41b7ca4a34
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-12-09 12:27:58 -0500

    separate tests based on new file structure

---
+++ test/workoutsummary_test.dart
@@ -0,0 +1,13 @@
+import 'dart:typed_data';
+
+import 'package:c2bluetooth/models/workoutsummary.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('can extract values from a workout summary byte list', () {
+    final summary = WorkoutSummary.fromBytes(Uint8List.fromList(
+        [0, 0, 0, 0, 128, 0, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]));
+    expect(summary.elapsedTime, 128);
+    expect(summary.distance, 255);
+  });
+}
