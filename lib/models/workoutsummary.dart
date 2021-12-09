1a5101582e2209206dcaaafeb5d7f8116cad7059
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-12-09 12:51:34 -0500

    add time date and avg spm to WorkoutSummary

--- lib/models/workoutsummary.dart
+++ lib/models/workoutsummary.dart
@@ -6,10 +6,16 @@ import '../helpers.dart';
 ///
 /// This takes care of dealing with byte endianness, combining multiple high and low bytes .etc so that applications using the data only have to deal with flutter [int] types
 class WorkoutSummary {
+  final int date;
+  final int time;
   final int elapsedTime;
   final int distance;
+  final int avgSPM;
 
   WorkoutSummary.fromBytes(Uint8List data)
-      : elapsedTime = bytesToInt(data.sublist(4, 7), Endian.little),
-        distance = bytesToInt(data.sublist(7, 10), Endian.little);
+      : date = bytesToInt(data.sublist(0, 2), Endian.little),
+        time = bytesToInt(data.sublist(2, 4), Endian.little),
+        elapsedTime = bytesToInt(data.sublist(4, 7), Endian.little),
+        distance = bytesToInt(data.sublist(7, 10), Endian.little),
+        avgSPM = bytesToInt(data.sublist(10, 11), Endian.little);
 }
