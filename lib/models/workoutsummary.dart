873701b4c3ea8f19e8dc47c53e75128c0cc93a9d
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-12-10 11:18:54 -0500

    add function to convert from concept2's time format

--- lib/models/workoutsummary.dart
+++ lib/models/workoutsummary.dart
@@ -6,8 +6,7 @@ import '../helpers.dart';
 ///
 /// This takes care of dealing with byte endianness, combining multiple high and low bytes .etc so that applications using the data only have to deal with flutter [int] types
 class WorkoutSummary {
-  final int date;
-  final int time;
+  final DateTime timestamp;
   final int elapsedTime;
   final double distance;
   final int avgSPM;
@@ -20,17 +19,27 @@ class WorkoutSummary {
   // final int workoutType;
 
   WorkoutSummary.fromBytes(Uint8List data)
-      : date = bytesToInt(data.sublist(0, 2), Endian.little),
-        time = bytesToInt(data.sublist(2, 4), Endian.little),
+      : timestamp = timeFromBytes(data.sublist(0, 4)),
         elapsedTime = bytesToInt(data.sublist(4, 7), Endian.little),
         distance = bytesToInt(data.sublist(7, 10), Endian.little) /
             10, //divide by 10 to convert to meters
         avgSPM = bytesToInt(data.sublist(10, 11), Endian.little);
   @override
   String toString() => "WorkoutSummary ("
-      "Date: $date, "
-      "Time: $time, "
+      "Timestamp: $timestamp, "
       "elapsedTime: $elapsedTime, "
       "distance: $distance, "
       "avgSPM: $avgSPM)";
+}
+
+DateTime timeFromBytes(Uint8List bytes) {
+  int date = bytesToInt(bytes.sublist(0, 1));
+
+  int day = (date & 0xF0) >> 4;
+  int month = date & 0x0F;
+  int year = bytesToInt(bytes.sublist(1, 2));
+  int minutes = bytesToInt(bytes.sublist(2, 3));
+  int hours = bytesToInt(bytes.sublist(3, 4));
+
+  return DateTime(year ~/ 2, month, day, hours, minutes);
 }
