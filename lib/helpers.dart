a8df23d5b370b805c34dee37be3e2f0f3b4ca093
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-12-10 11:20:30 -0500

    move TimefromBytes to helpers

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -16,3 +16,15 @@ int bytesToInt(Uint8List data, [Endian endian = Endian.big]) {
   }
   return result;
 }
+
+DateTime timeFromBytes(Uint8List bytes) {
+  int date = bytesToInt(bytes.sublist(0, 1));
+
+  int day = (date & 0xF0) >> 4;
+  int month = date & 0x0F;
+  int year = bytesToInt(bytes.sublist(1, 2));
+  int minutes = bytesToInt(bytes.sublist(2, 3));
+  int hours = bytesToInt(bytes.sublist(3, 4));
+
+  return DateTime(year ~/ 2, month, day, hours, minutes);
+}
