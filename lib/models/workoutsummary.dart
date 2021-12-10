de25b28b7f448b34f132242a5c9b85e252bb591b
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-12-10 11:28:31 -0500

    rename elapsedTime to workTime since that is what it appears to be

--- lib/models/workoutsummary.dart
+++ lib/models/workoutsummary.dart
@@ -7,7 +7,7 @@ import '../helpers.dart';
 /// This takes care of dealing with byte endianness, combining multiple high and low bytes .etc so that applications using the data only have to deal with flutter [int] types
 class WorkoutSummary {
   final DateTime timestamp;
-  final int elapsedTime;
+  final int workTime;
   final double distance;
   final int avgSPM;
   // final int endHeartRate;
@@ -20,7 +20,7 @@ class WorkoutSummary {
 
   WorkoutSummary.fromBytes(Uint8List data)
       : timestamp = timeFromBytes(data.sublist(0, 4)),
-        elapsedTime = bytesToInt(data.sublist(4, 7), Endian.little),
+        workTime = bytesToInt(data.sublist(4, 7), Endian.little),
         distance = bytesToInt(data.sublist(7, 10), Endian.little) /
             10, //divide by 10 to convert to meters
         avgSPM = bytesToInt(data.sublist(10, 11), Endian.little);
