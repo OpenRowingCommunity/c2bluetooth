4ce43d9a73a4654d387fb226aee4a333f602d8f5
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-12-10 13:42:03 -0500

    fix a forgotten instance of elapsedTime

--- test/workoutsummary_test.dart
+++ test/workoutsummary_test.dart
@@ -7,7 +7,7 @@ void main() {
   test('can extract values from a workout summary byte list', () {
     final summary = WorkoutSummary.fromBytes(Uint8List.fromList(
         [0, 0, 0, 0, 128, 0, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]));
-    expect(summary.elapsedTime, 128);
+    expect(summary.workTime, 128);
     expect(summary.distance, 25.5);
   });
 }
