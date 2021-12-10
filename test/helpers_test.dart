7ac055addf8fee8997dde63aac563599c6e50fb6
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-12-09 22:47:06 -0500

    remove unused import

--- test/helpers_test.dart
+++ test/helpers_test.dart
@@ -3,8 +3,6 @@ import 'dart:typed_data';
 import 'package:c2bluetooth/helpers.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:c2bluetooth/c2bluetooth.dart';
-
 void main() {
   test('can convert a byte list to an integer', () {
     final bytes = Uint8List.fromList([0, 0, 0, 128]);
