a5f3019a9ac2ba43d8c59bd019437a83c1cd6013
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-12-10 14:09:21 -0500

    update workDistance property in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -133,7 +133,7 @@ class _SimpleErgViewState extends State<SimpleErgView> {
 
     targetDevice!.monitorForWorkoutSummary().listen((summary) {
       setState(() {
-        displayText = "distance: ${summary.distance}";
+        displayText = "distance: ${summary.workDistance}";
         displayText2 = "datetime: ${summary.timestamp}";
         displayText3 = "sr: ${summary.avgSPM}";
       });
