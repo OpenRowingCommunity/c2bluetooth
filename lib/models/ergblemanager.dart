c1c003d9f725b60cf92a5c906db76d039c1a39a6
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-12-12 10:11:56 -0500

    move Ergometer and ErgBleManager classes to separate files and make the main file a way to explicitly define the public API

---
+++ lib/models/ergblemanager.dart
@@ -0,0 +1,32 @@
+import 'package:c2bluetooth/constants.dart' as Identifiers;
+import 'package:flutter_ble_lib/flutter_ble_lib.dart';
+import 'ergometer.dart';
+
+class ErgBleManager {
+  BleManager _manager = BleManager();
+
+  /// perform set up to get the Bluetooth client ready to scan for devices
+  void init() {
+    _manager.createClient();
+  }
+
+  /// Begin scanning for Ergs.
+  ///
+  /// This begins a scan for bluetooth devices with a filter applied so that only Concept2 Performance Monitors show up.
+  /// Bluetooth must be on and adequate permissions must be granted to the app for this to work.
+  Stream<Ergometer> startErgScan() {
+    return _manager.startPeripheralScan(uuids: [
+      Identifiers.C2_ROWING_BASE_UUID
+    ]).map((scanResult) => Ergometer.fromPeripheral(scanResult.peripheral));
+  }
+
+  /// Stops scanning for ergs
+  Future<void> stopErgScan() {
+    return _manager.stopPeripheralScan();
+  }
+
+  /// Clean up/destroy/deallocate resources so that they are availalble again
+  Future<void> destroy() {
+    return _manager.destroyClient();
+  }
+}
---
+++ test/ergblemanager_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import '../lib/models/ergblemanager.dart';
+
+void main() {
+  test('can obtain stream of ergometers present', () {
+    // final bytes = Uint8List.fromList([0, 0, 0, 128]);
+    // expect(bytesToInt(bytes), 128);
+    // expect(bytesToInt(bytes, Endian.little), 2147483648);
+  });
+
+  test('does not recognize non-concept2 devices', () {
+    // final bytes = Uint8List.fromList([0, 0, 0, 128]);
+    // expect(bytesToInt(bytes), 128);
+    // expect(bytesToInt(bytes, Endian.little), 2147483648);
+  });
+}
