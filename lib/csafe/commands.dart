8812bce6d9c1abed1594201d1f4b4411d603d6c0
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-01-30 20:57:41 -0500

    update setSplitDuration command to accept a more accurate type checking is done sooner via the type system

--- lib/csafe/commands.dart
+++ lib/csafe/commands.dart
@@ -1,11 +1,15 @@
 import 'package:c2bluetooth/csafe/datatypes.dart';
 import 'package:csafe_fitness/csafe_fitness.dart';
+import 'validators.dart';
 
 /// a list of Csafe Commands for concept2
 
 class CsafePMSetSplitDuration extends CsafeCommand {
-  CsafePMSetSplitDuration(ByteSerializable data) : super.long(0x05, 5, data) {
-    validateData(data, [validateType<Concept2IntegerWithUnits>()],
+  CsafePMSetSplitDuration(Concept2IntegerWithUnits duration)
+      : super.long(0x05, 5, duration) {
+    validateData(duration, [validateC2SplitGoal()], shouldThrow: true);
+  }
+}
         shouldThrow: true);
   }
 }
