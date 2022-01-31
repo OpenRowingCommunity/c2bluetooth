10ba0225bb11bfc4fc0384d4ff90fe3eb643e2b5
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-01-30 20:58:16 -0500

    add validators file

---
+++ lib/csafe/validators.dart
@@ -0,0 +1,15 @@
+import 'package:c2bluetooth/c2bluetooth.dart';
+import 'package:c2bluetooth/csafe/datatypes.dart';
+import 'package:csafe_fitness/csafe_fitness.dart';
+
+//TODO: validate data as a big endian IntegerWithUnits-like type with DurationType as the unit
+// maybe make a Concept2IntegerWithUnit subclass to handle this
+
+Validator validateC2SplitGoal() {
+  return validate(
+      (data) =>
+          (data is Concept2IntegerWithUnits) &&
+          (data.unit == DurationType.DISTANCE ||
+              data.unit == DurationType.TIME),
+      (data) => ArgumentError("Value provided must be in either a distance or a time unit"));
+}
