9b783624c08bfc621c765ae9de07fe9b1dae4909
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-02-13 00:57:16 -0500

    rearrange

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -4,10 +4,10 @@ import 'package:csafe_fitness/csafe_fitness.dart';
 
 DateTime timeFromBytes(Uint8List bytes) {
   int date = CsafeIntExtension.fromBytes(bytes.sublist(0, 2));
-  int year = date >> 9;
 
-  int day = (date >> 4) & 0x1F;
   int month = date & 0x0F;
+  int day = (date >> 4) & 0x1F;
+  int year = (date >> 9) & 0x7f;
 
   int minutes = CsafeIntExtension.fromBytes(bytes.sublist(2, 3));
   int hours = CsafeIntExtension.fromBytes(bytes.sublist(3, 4));
