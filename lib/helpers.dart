605f296a0cec3bc33e41010c0a83616557e08a6b
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-02-13 00:55:11 -0500

    simplfy the parsing a lot

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -3,13 +3,11 @@ import 'dart:typed_data';
 import 'package:csafe_fitness/csafe_fitness.dart';
 
 DateTime timeFromBytes(Uint8List bytes) {
-  int date = CsafeIntExtension.fromBytes(bytes.sublist(1, 2));
-  int year_bytes = CsafeIntExtension.fromBytes(bytes.sublist(0, 1));
-  int year = year_bytes >> 1;
+  int date = CsafeIntExtension.fromBytes(bytes.sublist(0, 2));
+  int year = date >> 9;
 
-  int day = ((year_bytes & 0x01) << 4) | ((date & 0xF0) >> 4);
+  int day = (date >> 4) & 0x1F;
   int month = date & 0x0F;
-  // year = year >> 1;
 
   int minutes = CsafeIntExtension.fromBytes(bytes.sublist(2, 3));
   int hours = CsafeIntExtension.fromBytes(bytes.sublist(3, 4));
