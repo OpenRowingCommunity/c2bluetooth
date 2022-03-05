4cdfceba6ed904bc2dc19fc57b010d09b8eb6ded
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-03-05 15:19:53 -0500

    extend CsafeCmdSetTime to validate the duration

--- lib/csafe/commands.dart
+++ lib/csafe/commands.dart
@@ -1,5 +1,6 @@
 import 'package:c2bluetooth/csafe/datatypes.dart';
 import 'package:csafe_fitness/csafe_fitness.dart';
+import 'package:csafe_fitness/commands.dart';
 import 'validators.dart';
 
 /// a list of Csafe Commands for concept2
@@ -10,6 +11,19 @@ class CsafePMSetSplitDuration extends CsafeCommand {
     validateData(duration, [validateC2SplitGoal()], shouldThrow: true);
   }
 }
+        shouldThrow: true);
+  }
+}
+
+/// A CSAFE command to set a workout time goal
+///
+/// This extends upon the Csafe version of the command in order to add checks for Concept2-specified limits.
+/// According to Concept2, time goals must be between 20 seconds  (inclusive) and 10 hours (exclusive).
+class C2CsafeCmdSetTime extends CsafeCmdSetTime {
+  static const _TEN_HOURS_IN_SECONDS = Duration.secondsPerHour * 10;
+  C2CsafeCmdSetTime(ByteSerializable data) : super(data) {
+    //if we are at this point, the superclass validators should have already run to confirm we have a valid time value
+    validateData(data, [validateTimeValue(20, _TEN_HOURS_IN_SECONDS)],
         shouldThrow: true);
   }
 }
