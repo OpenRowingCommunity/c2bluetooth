cc65456aed2d7ed297be15f91f129ecd842dbcdf
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-03-06 10:26:21 -0500

    rename time goal subclass again to remove c2 naming for consistency

--- lib/csafe/commands.dart
+++ lib/csafe/commands.dart
@@ -19,9 +19,9 @@ class CsafePMSetSplitDuration extends CsafeCommand {
 ///
 /// This extends upon the Csafe version of the command in order to add checks for Concept2-specified limits.
 /// According to Concept2, time goals must be between 20 seconds  (inclusive) and 10 hours (exclusive).
-class C2CsafeCmdSetTimeGoal extends CsafeCmdSetTime {
+class CsafeCmdSetTimeGoal extends CsafeCmdSetTime {
   static const _TEN_HOURS_IN_SECONDS = Duration.secondsPerHour * 10;
-  C2CsafeCmdSetTimeGoal(ByteSerializable data) : super(data) {
+  CsafeCmdSetTimeGoal(ByteSerializable data) : super(data) {
     //if we are at this point, the superclass validators should have already run to confirm we have a valid time value
     validateData(data, [validateTimeValue(20, _TEN_HOURS_IN_SECONDS)],
         shouldThrow: true);
