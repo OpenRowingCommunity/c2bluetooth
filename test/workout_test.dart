6cb56d79ee96484600c4e2a298c039b954eab942
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-03-17 07:51:17 -0400

    update tests

--- test/workout_test.dart
+++ test/workout_test.dart
@@ -6,13 +6,16 @@ import 'package:c2bluetooth/models/workout.dart';
 void main() {
   group("Workout - ", () {
     test('can get the correct workout type', () {
-      // final bytes = Uint8List.fromList([0, 0, 0, 128]);
+      // something prime or not divisible in a reasonably high (like 4) number of splits wont be assigned a split
       expect(Workout.single(WorkoutGoal.minutes(2)).getC2WorkoutType(),
           WorkoutType.FIXEDTIME_NOSPLITS);
       expect(Workout.single(WorkoutGoal.minutes(6)).getC2WorkoutType(),
           WorkoutType.FIXEDTIME_SPLITS);
       expect(Workout.single(WorkoutGoal.meters(2000)).getC2WorkoutType(),
           WorkoutType.FIXEDDIST_SPLITS);
+      // something prime or not divisible in a reasonably low number of splits wont be assigned a split
+      expect(Workout.single(WorkoutGoal.meters(59)).getC2WorkoutType(),
+          WorkoutType.FIXEDDIST_NOSPLITS);
       // 2k on 500 off
       expect(
           Workout.intervals(
