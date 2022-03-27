401d16d65a6b692b060eba93d3c9484f824d9865
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-03-27 16:06:56 -0400

    refactor WorkoutSummary to use futures

--- lib/data/workoutsummary.dart
+++ lib/data/workoutsummary.dart
@@ -1,4 +1,5 @@
 import 'dart:typed_data';
+import 'dart:async';
 
 import 'package:csafe_fitness/csafe_fitness.dart';
 
@@ -9,27 +10,49 @@ import 'package:c2bluetooth/enums.dart';
 ///
 /// This takes care of processesing the raw byte data from workout summary characteristics into easily accessible fields. This class also takes care of things like byte endianness, combining multiple high and low bytes .etc, allowing applications to access things in terms of flutter native types.
 class WorkoutSummary {
-  final DateTime timestamp;
-  final double workTime;
-  final double workDistance;
-  final int avgSPM;
-  final int endHeartRate;
-  final int avgHeartRate;
-  final int minHeartRate;
-  final int maxHeartRate;
-  final int avgDragFactor;
+  Completer<DateTime> _timestamp = new Completer<DateTime>();
+  Completer<double> _workTime = new Completer<double>();
+  Completer<double> _workDistance = new Completer<double>();
+  Completer<int> _avgSPM = new Completer<int>();
+  Completer<int> _endHeartRate = new Completer<int>();
+  Completer<int> _avgHeartRate = new Completer<int>();
+  Completer<int> _minHeartRate = new Completer<int>();
+  Completer<int> _maxHeartRate = new Completer<int>();
+  Completer<int> _avgDragFactor = new Completer<int>();
+  Completer<int> _recoveryHeartRate = new Completer<int>();
+  Completer<WorkoutType> _workoutType = new Completer<WorkoutType>();
+  Completer<double> _avgPace = new Completer<double>();
+  Completer<IntervalType> _intervalType = new Completer<IntervalType>();
+  Completer<int> _intervalSize = new Completer<int>();
+  Completer<int> _intervalCount = new Completer<int>();
+  Completer<int> _totalCalories = new Completer<int>();
+  Completer<int> _watts = new Completer<int>();
+  Completer<int> _totalRestDistance = new Completer<int>();
+  Completer<int> _intervalRestTime = new Completer<int>();
+  Completer<int> _avgCalories = new Completer<int>();
+
+  // external getters for clients to get futures for the data they want
+  Future<DateTime> get timestamp => _timestamp.future;
+  Future<double> get workTime => _workTime.future;
+  Future<double> get workDistance => _workDistance.future;
+  Future<int> get avgSPM => _avgSPM.future;
+  Future<int> get endHeartRate => _endHeartRate.future;
+  Future<int> get avgHeartRate => _avgHeartRate.future;
+  Future<int> get minHeartRate => _minHeartRate.future;
+  Future<int> get maxHeartRate => _maxHeartRate.future;
+  Future<int> get avgDragFactor => _avgDragFactor.future;
   //recoveryHeartRate is sent as an amended packet later. zero is not valid
-  int? recoveryHeartRate;
-  final WorkoutType workoutType;
-  final double avgPace;
-  IntervalType? intervalType;
-  int? intervalSize;
-  int? intervalCount;
-  int? totalCalories;
-  int? watts;
-  int? totalRestDistance;
-  int? intervalRestTime;
-  int? avgCalories;
+  Future<int> get recoveryHeartRate => _recoveryHeartRate.future;
+  Future<WorkoutType> get workoutType => _workoutType.future;
+  Future<double> get avgPace => _avgPace.future;
+  Future<IntervalType> get intervalType => _intervalType.future;
+  Future<int> get intervalSize => _intervalSize.future;
+  Future<int> get intervalCount => _intervalCount.future;
+  Future<int> get totalCalories => _totalCalories.future;
+  Future<int> get watts => _watts.future;
+  Future<int> get totalRestDistance => _totalRestDistance.future;
+  Future<int> get intervalRestTime => _intervalRestTime.future;
+  Future<int> get avgCalories => _avgCalories.future;
 
   /// Construct a WorkoutSummary from the bytes returned from the erg
   WorkoutSummary.fromBytes(Uint8List data)
