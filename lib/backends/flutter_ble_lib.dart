f32087d5dff8280c2f72e46313a5ac570ae12862
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-04-15 07:48:07 -0400

    add empty BluetoothDevice implementation

--- lib/backends/flutter_ble_lib.dart
+++ lib/backends/flutter_ble_lib.dart
@@ -1,4 +1,5 @@
 import 'package:c2bluetooth/backends/interface/bluetoothclient.dart';
+import 'package:c2bluetooth/backends/interface/bluetoothdevice.dart';
 
 /// an implementation of the ble backend interface for FlutterBleLib and libraries based on it.
 import 'package:flutter_ble_lib_ios_15/flutter_ble_lib.dart';
@@ -21,4 +22,20 @@ class FlutterBleLibClient extends BluetoothClient {
   Future<void> destroyClient() {
     return _client.destroyClient();
   }
+}
+
+class FlutterBleLibDevice extends BluetoothDevice {
+  connect() {}
+
+  discoverAllServicesAndCharacteristics() {}
+
+  Future<void> disconnectOrCancelConnection() {}
+
+  monitorCharacteristic(String c2_rowing_primary_service_uuid,
+      String c2_rowing_end_of_workout_summary_characteristic_uuid) {}
+
+  observeConnectionState() {}
+  writeCharacteristic() {}
+
+  readCharacteristic() {}
 }
