1fc4cd07bb83bda015627cee73fa209c9e6b6b5e
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-04-15 07:40:58 -0400

    add more function stubs

--- lib/backends/interface/bluetoothdevice.dart
+++ lib/backends/interface/bluetoothdevice.dart
@@ -7,5 +7,11 @@ abstract class BluetoothDevice {
 
   Future<void> disconnectOrCancelConnection() {}
 
-  monitorCharacteristic(String c2_rowing_primary_service_uuid, String c2_rowing_end_of_workout_summary_characteristic_uuid) {}
+  monitorCharacteristic(String c2_rowing_primary_service_uuid,
+      String c2_rowing_end_of_workout_summary_characteristic_uuid) {}
+
+  observeConnectionState() {}
+  writeCharacteristic() {}
+
+  readCharacteristic() {}
 }
