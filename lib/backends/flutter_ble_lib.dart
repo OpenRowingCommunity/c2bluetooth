084afec96adee59e2171caaeb499ee1b08feecec
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-04-15 07:45:27 -0400

    passthrough method calls to flutter_ble_lib BleManager

--- lib/backends/flutter_ble_lib.dart
+++ lib/backends/flutter_ble_lib.dart
@@ -3,4 +3,22 @@ import 'package:c2bluetooth/backends/interface/bluetoothclient.dart';
 /// an implementation of the ble backend interface for FlutterBleLib and libraries based on it.
 import 'package:flutter_ble_lib_ios_15/flutter_ble_lib.dart';
 
-class FlutterBleLibClient extends BluetoothClient {}
+class FlutterBleLibClient extends BluetoothClient {
+  BleManager _client = BleManager();
+
+  void createClient() {
+    _client.createClient();
+  }
+
+  startPeripheralScan({List<String>? uuids}) {
+    _client.startPeripheralScan(uuids: uuids ?? []);
+  }
+
+  Future<void> stopPeripheralScan() {
+    return _client.stopPeripheralScan();
+  }
+
+  Future<void> destroyClient() {
+    return _client.destroyClient();
+  }
+}
