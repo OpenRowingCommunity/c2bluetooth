e9b8cbb3e4e7ca83f6e73a031aaec101880d852d
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-04-15 21:17:34 -0400

    import typed data

--- lib/backends/flutter_ble_lib.dart
+++ lib/backends/flutter_ble_lib.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:c2bluetooth/backends/interface/bluetoothclient.dart';
 import 'package:c2bluetooth/backends/interface/bluetoothdevice.dart';
 import 'package:c2bluetooth/backends/interface/bluetoothscanresult.dart';
