035a1775efbe49e231acf3fc7542bd7b57ff34e6
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-05-09 21:17:46 -0400

    bring in status data stub classes

---
+++ src/packets/statusdata.dart
@@ -0,0 +1,33 @@
+import 'dart:typed_data';
+
+import 'package:csafe_fitness/csafe_fitness.dart';
+
+import './base.dart';
+
+class StatusData extends DurationstampedData {
+  double distance;
+
+  StatusData.fromBytes(Uint8List data)
+      : distance = CsafeIntExtension.fromBytes(data.sublist(3, 6),
+                endian: Endian.little) /
+            10,
+        super.fromBytes(data);
+}
+
+class StatusData1 extends DurationstampedData {
+  double speed;
+
+  StatusData1.fromBytes(Uint8List data)
+      : speed = CsafeIntExtension.fromBytes(data.sublist(3, 5),
+                endian: Endian.little) /
+            1000,
+        super.fromBytes(data);
+}
+
+class StatusData2 extends DurationstampedData {
+  int intervalCount;
+
+  StatusData2.fromBytes(Uint8List data)
+      : intervalCount = data.elementAt(3),
+        super.fromBytes(data);
+}
