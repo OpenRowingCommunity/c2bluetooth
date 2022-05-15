6d905e2dfdfe2d48f53972398c14c891694a3da7
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-05-15 12:26:40 -0400

    create a basic passthrough wrapper for a stream controller

---
+++ lib/models/cachedstreamcontroller.dart
@@ -0,0 +1,87 @@
+import 'dart:async';
+
+/// A wrapper around StreamController to aid in delivering data from bluetooth to the user
+class CachedStreamController<T> implements StreamController<T> {
+  StreamController<T> _controller;
+
+  ///called when the controller loses its last subscriber
+  ///https://dart.dev/articles/libraries/creating-streams#using-a-streamcontroller
+  @override
+  FutureOr<void> Function()? get onCancel => _controller.onCancel;
+
+  set onCancel(FutureOr<void> Function()? newValue) {
+    _controller.onCancel = newValue;
+  }
+
+  ///called when the stream gets its first subscriber
+  ///https://dart.dev/articles/libraries/creating-streams#using-a-streamcontroller
+  @override
+  void Function()? get onListen => _controller.onListen;
+
+  set onListen(Function()? newValue) {
+    _controller.onListen = newValue;
+  }
+
+  @override
+  void Function()? get onPause => _controller.onPause;
+
+  set onPause(Function()? newValue) {
+    _controller.onPause = newValue;
+  }
+
+  @override
+  void Function()? get onResume => _controller.onResume;
+
+  set onResume(Function()? newValue) {
+    _controller.onResume = newValue;
+  }
+
+  CachedStreamController(
+      {void onListen()?,
+      void onPause()?,
+      void onResume()?,
+      FutureOr<void> onCancel()?})
+      : _controller = new StreamController(
+            onListen: onListen,
+            onPause: onPause,
+            onResume: onResume,
+            onCancel: onCancel);
+
+  @override
+  void add(T event) {
+    _controller.add(event);
+  }
+
+  @override
+  void addError(Object error, [StackTrace? stackTrace]) {
+    _controller.addError(error, stackTrace);
+  }
+
+  @override
+  Future addStream(Stream<T> source, {bool? cancelOnError}) {
+    return _controller.addStream(source, cancelOnError: cancelOnError);
+  }
+
+  @override
+  Future close() {
+    return _controller.close();
+  }
+
+  @override
+  Future get done => _controller.done;
+
+  @override
+  bool get hasListener => _controller.hasListener;
+
+  @override
+  bool get isClosed => _controller.isClosed;
+
+  @override
+  bool get isPaused => _controller.isPaused;
+
+  @override
+  StreamSink<T> get sink => _controller.sink;
+
+  @override
+  Stream<T> get stream => _controller.stream;
+}
