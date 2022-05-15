0551f9a919bb07372e5dbac8926c65d4a3dd5ece
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-05-15 14:36:02 -0400

    uncomment packet type parsing function and update types

--- lib/src/dataplex.dart
+++ lib/src/dataplex.dart
@@ -5,6 +5,7 @@ import 'package:flutter_ble_lib_ios_15/flutter_ble_lib.dart';
 
 import './packets/statusdata.dart';
 import './packets/strokedata.dart';
+import 'packets/base.dart';
 
 /// Handles mapping between data coming from bluetooth notfications and the data the user requested.
 /// This gives c2bluetooth a layer of flexibility and decouples the incoming bluetooth data from the output going to an application so that c2bluetooth has space to potentially optimize the data being used
@@ -48,44 +49,44 @@ class Dataplex {
   }
 
   void _readPacket(Uint8List data) {}
-  // String _packetType(Uint8List data) {
-  //   switch (data[0]) {
-  //     case 0x31:
-  //       StatusData parsed = StatusData.fromBytes(data.sublist(1));
-  //       print(parsed.elapsedTime.toString());
-  //       print("distance ${parsed.distance.toString()}");
+  Concept2CharacteristicData _identifyPacketType(Uint8List data) {
+    switch (data[0]) {
+      case 0x31:
+        StatusData parsed = StatusData.fromBytes(data.sublist(1));
+        print(parsed.elapsedTime.toString());
+        print("distance ${parsed.distance.toString()}");
 
-  //       return "status";
-  //     case 0x32:
-  //       StatusData1 parsed = StatusData1.fromBytes(data.sublist(1));
-  //       print(parsed.elapsedTime.toString());
-  //       print("speed ${parsed.speed.toString()}");
+        return parsed;
+      case 0x32:
+        StatusData1 parsed = StatusData1.fromBytes(data.sublist(1));
+        print(parsed.elapsedTime.toString());
+        print("speed ${parsed.speed.toString()}");
 
-  //       return "Status 1";
-  //     case 0x33:
-  //       StatusData2 parsed = StatusData2.fromBytes(data.sublist(1));
-  //       print(parsed.elapsedTime.toString());
-  //       // print("intervalcount ${parsed.intervalCount.toString()}");
-  //       return "Status 2";
-  //     case 0x35:
-  //       return "stroke data";
-  //     case 0x36:
-  //       return "stroke data 1";
-  //     case 0x37:
-  //       return "split/interval data";
-  //     case 0x38:
-  //       return "split/interval data 2";
-  //     case 0x39:
-  //       return "workout data";
-  //     case 0x3A:
-  //       return "workout data 1";
-  //     case 0x3C:
-  //       print(data);
-  //       return "workout data 2";
-  //     default:
-  //       return "Unknown";
-  //   }
-  // }
+        return parsed;
+      case 0x33:
+        StatusData2 parsed = StatusData2.fromBytes(data.sublist(1));
+        print(parsed.elapsedTime.toString());
+        // print("intervalcount ${parsed.intervalCount.toString()}");
+        return parsed;
+      case 0x35:
+        return "stroke data";
+      case 0x36:
+        return "stroke data 1";
+      case 0x37:
+        return "split/interval data";
+      case 0x38:
+        return "split/interval data 2";
+      case 0x39:
+        return "workout data";
+      case 0x3A:
+        return "workout data 1";
+      case 0x3C:
+        print(data);
+        return "workout data 2";
+      default:
+        return "Unknown";
+    }
+  }
 
   void dispose() {
     // clear current subscriptions
