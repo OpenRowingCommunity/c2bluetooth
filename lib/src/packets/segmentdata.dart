4d188e7e3b260f1302feba800da9ce265f838659
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-05-22 13:48:07 -0400

    parse more datapoints out of segmentData

--- lib/src/packets/segmentdata.dart
+++ lib/src/packets/segmentdata.dart
@@ -7,7 +7,26 @@ import './base.dart';
 ///
 /// Segment refers to the concept of "split or interval" from Concept2's specification since the two are mutually exclusive
 class SegmentData extends ElapsedtimeStampedData {
-  SegmentData.fromBytes(Uint8List data) : super.fromBytes(data);
+  double distance;
+  // double segmentTime;
+  int segmentDistance;
+  int segmentRestTime;
+  int segmentRestDistance;
+  int segmentNumber;
+
+  SegmentData.fromBytes(Uint8List data)
+      : distance = CsafeIntExtension.fromBytes(data.sublist(3, 6),
+                endian: Endian.little) /
+            10,
+        // segmentTime = 6,9,
+        segmentDistance = CsafeIntExtension.fromBytes(data.sublist(9, 12),
+            endian: Endian.little),
+        segmentRestDistance = CsafeIntExtension.fromBytes(data.sublist(12, 14),
+            endian: Endian.little),
+        segmentRestTime = CsafeIntExtension.fromBytes(data.sublist(14, 16),
+            endian: Endian.little),
+        segmentNumber = data.elementAt(18),
+        super.fromBytes(data);
 }
 
 /// Represents a packet containing data for a "Segment" of a workout.
