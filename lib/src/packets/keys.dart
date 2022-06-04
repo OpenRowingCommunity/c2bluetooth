71948421927be500a0285f8f3b074d5171017c8f
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-06-04 11:47:31 -0400

    start adding data keys to a constants file

---
+++ lib/src/packets/keys.dart
@@ -0,0 +1,20 @@
+const ELAPSED_TIME_KEY = "workout.timestamp";
+
+const WORKOUT_TIMESTAMP_KEY = "workout.timestamp";
+const WORKOUT_AVG_SPM_KEY = "workout.stroke_rate.average";
+const WORKOUT_LAST_HR_KEY = "workout.heart_rate.last";
+const WORKOUT_AVG_HR_KEY = "workout.heart_rate.average";
+const WORKOUT_MIN_HR_KEY = "workout.heart_rate.min";
+const WORKOUT_MAX_HR_KEY = "workout.heart_rate.max";
+const WORKOUT_AVG_PACE_KEY = "workout.pace.average";
+const WORKOUT_AVG_DRAGFACTOR_KEY = "workout.drag_factor.average";
+const WORKOUT_RECOVERY_HR_KEY = "workout.heart_rate.recovery";
+
+const WORKOUT_SEGMENT_COUNT_KEY = "workout.segment_count";
+const WORKOUT_SEGMENT_SIZE_KEY = "workout.segment_size";
+const WORKOUT_CALORIES_KEY = "workout.calories";
+const WORKOUT_WATTS_KEY = "workout.watts";
+// rests are only applicable for intervals workouts
+const WORKOUT_REST_DISTANCE_KEY = "workout.rest_distance";
+const WORKOUT_REST_TIME_KEY = "workout.rest_time";
+const WORKOUT_AVG_CALORIES_KEY = "workout.calories.average";
