c6e1b27e161ace37743a83ba2d580ed09c158eaa
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-06-05 17:01:27 -0400

    fix endianness of the older date unit tests

--- test/extensions_test.dart
+++ test/extensions_test.dart
@@ -6,16 +6,14 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group("Concept2DateExtension - ", () {
     test('converting a time from bytes', () {
-      // TODO: fixme, my endianness is backwards
-      final bytes = Uint8List.fromList([42, 156, 1, 14]); //date 10908
+      final bytes = Uint8List.fromList([156, 42, 1, 14]); //date 10908
       expect(
           Concept2DateExtension.fromBytes(bytes), DateTime(2021, 12, 9, 14, 1));
       // expect(CsafeIntExtension.fromBytes(bytes, Endian.little), 2147483648);
     });
 
     test('converting another time from bytes', () {
-      // TODO: fixme, my endianness is backwards
-      final bytes = Uint8List.fromList([45, 241, 1, 14]);
+      final bytes = Uint8List.fromList([241, 45, 1, 14]);
       expect(
           Concept2DateExtension.fromBytes(bytes), DateTime(2022, 1, 31, 14, 1));
       // expect(CsafeIntExtension.fromBytes(bytes, Endian.little), 2147483648);
