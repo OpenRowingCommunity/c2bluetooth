0fe350c6ab6e0595a58f46aef402aa8f19e6d3e6
Adrian Edwards <adrian@adriancedwards.com>
2025-04-20 12:26:06 -0400

    finish reverting WorkoutSummary away from futures

--- lib/data/workoutsummary.dart
+++ lib/data/workoutsummary.dart
@@ -36,35 +36,43 @@ class WorkoutSummary extends TimestampedData {
   int minHeartRate;
   int maxHeartRate;
   int avgDragFactor;
-  int recoveryHeartRate;
+  late int recoveryHeartRate;
   WorkoutType workoutType;
   double avgPace;
 
   /// Construct a WorkoutSummary from the bytes returned from the erg
-  WorkoutSummary.fromBytes(Uint8List data) : super.fromBytes(data) {
-    _workTime =
-        CsafeIntExtension.fromBytes(data.sublist(4, 7), endian: Endian.little) /
-            100; //divide by 100 to convert to seconds
-    _workDistance = CsafeIntExtension.fromBytes(data.sublist(7, 10),
-            endian: Endian.little) /
-        10; //divide by 10 to convert to meters
-    _avgSPM = data.elementAt(10);
-    _endHeartRate = data.elementAt(11);
-    _avgHeartRate = data.elementAt(12);
-    _minHeartRate = data.elementAt(13);
-    _maxHeartRate = data.elementAt(14);
-    _avgDragFactor = data.elementAt(15);
+  WorkoutSummary.fromBytes(Uint8List data)
+      : workTime = CsafeIntExtension.fromBytes(data.sublist(4, 7),
+                endian: Endian.little) /
+            100, //divide by 100 to convert to seconds
+        workDistance = CsafeIntExtension.fromBytes(data.sublist(7, 10),
+                endian: Endian.little) /
+            10, //divide by 10 to convert to meters
+        avgSPM = data.elementAt(10),
+        endHeartRate = data.elementAt(11),
+        avgHeartRate = data.elementAt(12),
+        minHeartRate = data.elementAt(13),
+        maxHeartRate = data.elementAt(14),
+        avgDragFactor = data.elementAt(15),
+        workoutType = WorkoutTypeExtension.fromInt(data.elementAt(17)),
+        avgPace = CsafeIntExtension.fromBytes(data.sublist(18, 20),
+                endian: Endian.little) /
+            10,
+        super.fromBytes(data) {
     //recovery heart rate here
     int recHRVal = data.elementAt(16);
     // 0 is not a valid value here according to the spec
     if (recHRVal > 0) {
-      _recoveryHeartRate = recHRVal;
+      recoveryHeartRate = recHRVal;
     }
-    _workoutType = WorkoutTypeExtension.fromInt(data.elementAt(17));
-    _avgPace = CsafeIntExtension.fromBytes(data.sublist(18, 20),
-            endian: Endian.little) /
-        10;
   }
+
+  @override
+  String toString() => "WorkoutSummary ("
+      "Timestamp: $timestamp, "
+      "elapsedTime: $workTime, "
+      "distance: $workDistance, "
+      "avgSPM: $avgSPM)";
 }
 
 class WorkoutSummary2 {
@@ -77,34 +85,27 @@ class WorkoutSummary2 {
   int intervalRestTime;
   int avgCalories;
 
-  WorkoutSummary2.fromBytes(Uint8List data) {
-    // if (data.length > 20) {
-    //   var timestamp2 = Concept2DateExtension.fromBytes(data.sublist(20, 24));
-    //   if (timestamp != timestamp2) {
-    //     throw ArgumentError(
-    //         "Bytes passed to WorkoutSummary from multiple characteristics must have the same timestamp");
-    //   }
+  WorkoutSummary2.fromBytes(Uint8List data)
+      :
+        // if (data.length > 20) {
+        //   var timestamp2 = Concept2DateExtension.fromBytes(data.sublist(20, 24));
+        //   if (timestamp != timestamp2) {
+        //     throw ArgumentError(
+        //         "Bytes passed to WorkoutSummary from multiple characteristics must have the same timestamp");
+        //   }
 
-    intervalType = IntervalTypeExtension.fromInt(data.elementAt(4));
-    intervalSize =
-        CsafeIntExtension.fromBytes(data.sublist(5, 7), endian: Endian.little);
-    intervalCount = data.elementAt(7);
-    totalCalories =
-        CsafeIntExtension.fromBytes(data.sublist(8, 10), endian: Endian.little);
-    watts = CsafeIntExtension.fromBytes(data.sublist(10, 12),
-        endian: Endian.little);
-    totalRestDistance = CsafeIntExtension.fromBytes(data.sublist(12, 15),
-        endian: Endian.little);
-    intervalRestTime = CsafeIntExtension.fromBytes(data.sublist(15, 17),
-        endian: Endian.little);
-    avgCalories = CsafeIntExtension.fromBytes(data.sublist(17, 19),
-        endian: Endian.little);
-  }
-
-  @override
-  String toString() => "WorkoutSummary ("
-      "Timestamp: $timestamp, "
-      "elapsedTime: $workTime, "
-      "distance: $workDistance, "
-      "avgSPM: $avgSPM)";
+        intervalType = IntervalTypeExtension.fromInt(data.elementAt(4)),
+        intervalSize = CsafeIntExtension.fromBytes(data.sublist(5, 7),
+            endian: Endian.little),
+        intervalCount = data.elementAt(7),
+        totalCalories = CsafeIntExtension.fromBytes(data.sublist(8, 10),
+            endian: Endian.little),
+        watts = CsafeIntExtension.fromBytes(data.sublist(10, 12),
+            endian: Endian.little),
+        totalRestDistance = CsafeIntExtension.fromBytes(data.sublist(12, 15),
+            endian: Endian.little),
+        intervalRestTime = CsafeIntExtension.fromBytes(data.sublist(15, 17),
+            endian: Endian.little),
+        avgCalories = CsafeIntExtension.fromBytes(data.sublist(17, 19),
+            endian: Endian.little) {}
 }
