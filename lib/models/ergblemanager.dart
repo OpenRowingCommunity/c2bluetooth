02ce312751f678ad6b472a3b10752568e94e48e6
Bertrand Esperou <bertrand.esperou@gmail.com>
2025-04-26 07:26:08 +0000

    explicitely telling the output are Ergometer objects

--- lib/models/ergblemanager.dart
+++ lib/models/ergblemanager.dart
@@ -14,7 +14,8 @@ class ErgBleManager {
   Stream<Ergometer> startErgScan() {
     return _manager.scanForDevices(withServices: [
       Uuid.parse(Identifiers.C2_ROWING_BASE_UUID)
-    ]).map((scanResult) => Ergometer(scanResult, bleClient: _manager));
+    ]).map<Ergometer>(
+        (scanResult) => Ergometer(scanResult, bleClient: _manager));
   }
 
   /// Clean up/destroy/deallocate resources so that they are availalble again
