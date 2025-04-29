1fd745c8df9601e9f37bd227bb17f793b39ce497
Bertrand Esperou <bertrand.esperou@gmail.com>
2025-04-29 12:11:21 +0000

    Allow testing when using a mock in place of the reactive package

--- lib/models/ergblemanager.dart
+++ lib/models/ergblemanager.dart
@@ -1,9 +1,17 @@
 import 'package:c2bluetooth/constants.dart' as Identifiers;
+import 'package:flutter/foundation.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'ergometer.dart';
 
 class ErgBleManager {
-  final _manager = FlutterReactiveBle();
+  final FlutterReactiveBle _manager;
+
+  ErgBleManager() : _manager = FlutterReactiveBle();
+
+  /// Allow [ErgBleManager] to be tested using a Mocked bluetooth client
+  @visibleForTesting
+  ErgBleManager.withDependency({FlutterReactiveBle? bleClient})
+      : _manager = bleClient ?? FlutterReactiveBle();
 
   /// Begin scanning for Ergs.
   ///
