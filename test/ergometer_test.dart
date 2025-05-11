427f36e093bc482fec6fb607c7a71df63653d521
Bertrand Esperou <bertrand.esperou@gmail.com>
2025-05-11 07:03:48 +0000

    use streamcontrollers to be more flexible during testing

--- test/ergometer_test.dart
+++ test/ergometer_test.dart
@@ -1,4 +1,6 @@
-import 'package:c2bluetooth/models/ergometer.dart';
+import 'dart:async';
+
+import 'package:c2bluetooth/c2bluetooth.dart';
 import 'package:c2bluetooth/src/dataplex.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
@@ -12,41 +14,51 @@ class FakeQualifiedCharacteristic extends Fake
 
 class FakeDataplex extends Fake implements Dataplex {}
 
+late MockFlutterReactiveBle mockBle;
+late StreamController<List<int>> characteristicController;
+late StreamController<ConnectionStateUpdate> deviceConnectionController;
+late DiscoveredDevice device = DiscoveredDevice(
+    id: 'deviceId',
+    name: 'deviceName',
+    serviceData: {},
+    manufacturerData: Uint8List.fromList([1, 1, 1, 1]),
+    rssi: 90,
+    serviceUuids: []);
 void main() {
   group('Bluetooth tests', () {
-    setUp(() {
-      registerFallbackValue(Stream.value(ConnectionStateUpdate(
-        deviceId: 'fallback',
-        connectionState: DeviceConnectionState.disconnected,
-        failure: null,
-      )));
-      registerFallbackValue(DiscoveredDevice(
-          id: 'deviceId',
-          name: 'deviceName',
-          serviceData: {},
-          manufacturerData: Uint8List.fromList([1, 1, 1, 1]),
-          rssi: 90,
-          serviceUuids: []));
-      registerFallbackValue(FakeQualifiedCharacteristic());
-      registerFallbackValue(Stream<List<int>>.fromIterable([
-        [1, 1, 1],
-        [2, 2, 2]
-      ]));
+    setUpAll(() {
+      // Fallback values
       registerFallbackValue(QualifiedCharacteristic(
           characteristicId: Uuid.parse('c5cc5bf5-2bd1-4d1a-939a-5e15fb9b81a1'),
           serviceId: Uuid.parse('c5cc5bf5-2bd1-4d1a-939a-5e15fb9b81a2'),
-          deviceId: 'deviceId'));
+          deviceId: device.id));
+    });
+    setUp(() {
+      mockBle = MockFlutterReactiveBle();
+      // Mock ReactiveBle methods using streamcontrollers
+      characteristicController =
+          StreamController<List<int>>.broadcast(sync: true);
+      deviceConnectionController =
+          StreamController<ConnectionStateUpdate>.broadcast(sync: true);
+      when(
+        () => mockBle.connectToDevice(
+          id: any(named: 'id'),
+          connectionTimeout: any(named: 'connectionTimeout'),
+        ),
+      ).thenAnswer((_) => deviceConnectionController.stream);
+      when(() => mockBle.connectedDeviceStream)
+          .thenAnswer((_) => deviceConnectionController.stream);
+      when(() =>
+              mockBle.subscribeToCharacteristic(any<QualifiedCharacteristic>()))
+          .thenAnswer((_) => characteristicController.stream);
+    });
+    tearDownAll(() {
+      deviceConnectionController.close();
+      characteristicController.close();
     });
 
     test('Ensure DeviceConnectionState to ErgometerConnectionState translation',
         () {
-      DiscoveredDevice device = DiscoveredDevice(
-          id: 'deviceId',
-          name: 'deviceName',
-          serviceData: {},
-          manufacturerData: Uint8List.fromList([1, 1, 1, 1]),
-          rssi: 90,
-          serviceUuids: []);
       final fakeConnectionUpdates = Stream<ConnectionStateUpdate>.fromIterable([
         ConnectionStateUpdate(
           deviceId: 'deviceId',
@@ -67,10 +79,10 @@ void main() {
             connectionState: DeviceConnectionState.disconnected,
             failure: null)
       ]);
-      final mockBle = MockFlutterReactiveBle();
       final erg = Ergometer(device, bleClient: mockBle);
-      when(() => mockBle.connectedDeviceStream)
-          .thenAnswer((_) => fakeConnectionUpdates);
+      fakeConnectionUpdates.forEach(deviceConnectionController.add);
+      StreamSubscription<ErgometerConnectionState> _connection =
+          erg.connectAndDiscover().listen((_) {});
       expect(
           erg.getMonitorConnectionState,
           emitsInOrder([
@@ -79,23 +91,14 @@ void main() {
             ErgometerConnectionState.disconnected,
             ErgometerConnectionState.disconnected
           ]));
-      verifyNever(() => mockBle.connectToDevice(
-            id: 'deviceId',
-            connectionTimeout: any(named: 'connectionTimeout'),
-          ));
+      _connection.cancel();
     });
     test('Retrieve ErgometerConnectionState status during connection', () {
-      // dummy  discovered device
-      DiscoveredDevice device = DiscoveredDevice(
-          id: 'deviceId',
-          name: 'deviceName',
-          serviceData: {},
-          manufacturerData: Uint8List.fromList([1, 1, 1, 1]),
-          rssi: 90,
-          serviceUuids: []);
       final fakeSubscriptionChar = Stream<List<int>>.fromIterable([
-        [1, 1, 1],
-        [2, 2, 2]
+        // Each StatusData1 is 18 bytes
+        [0x31, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 0 m
+        [0x31, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 1 m
+        [0x31, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 2 m
       ]);
       final fakeConnectionUpdates = Stream<ConnectionStateUpdate>.fromIterable([
         ConnectionStateUpdate(
@@ -109,38 +112,118 @@ void main() {
           failure: null,
         )
       ]);
-      final mockBle = MockFlutterReactiveBle();
       final erg = Ergometer(device, bleClient: mockBle);
-      when(() => mockBle.connectedDeviceStream)
-          .thenAnswer((_) => fakeConnectionUpdates);
-      when(
-        () => mockBle.connectToDevice(
-          id: any(named: 'id'),
-          connectionTimeout: any(named: 'connectionTimeout'),
-        ),
-      ).thenAnswer((_) => fakeConnectionUpdates);
-      when(() => mockBle.subscribeToCharacteristic(any()))
-          .thenAnswer((_) => fakeSubscriptionChar);
+      fakeConnectionUpdates.forEach(deviceConnectionController.add);
+      fakeSubscriptionChar.forEach(characteristicController.add);
       expect(
           erg.connectAndDiscover(),
           emitsInOrder([
             ErgometerConnectionState.connecting,
             ErgometerConnectionState.connected
           ]));
+      // Connection should happen once
       verify(() => mockBle.connectToDevice(
-            id: 'deviceId',
+            id: device.id,
             connectionTimeout: any(named: 'connectionTimeout'),
           )).called(1);
+      // Subscribed to the two defaut subscriptions:
+      // - Identifiers.C2_ROWING_PRIMARY_SERVICE_UUID,
+      // - Identifiers.C2_ROWING_MULTIPLEXED_INFORMATION_CHARACTERISTIC_UUID,
+      verify(() =>
+              mockBle.subscribeToCharacteristic(any<QualifiedCharacteristic>()))
+          .called(2);
     });
-  });
-  test('instantiate from a peripheral', () {
-    // final bytes = Uint8List.fromList([0, 0, 0, 128]);
-    // expect(CsafeIntExtension.fromBytes(bytes), 128);
-    // expect(CsafeIntExtension.fromBytes(bytes, Endian.little), 2147483648);
-  });
-  test('can provide WorkoutSummary data', () {
-    // final bytes = Uint8List.fromList([0, 0, 0, 128]);
-    // expect(CsafeIntExtension.fromBytes(bytes), 128);
-    // expect(CsafeIntExtension.fromBytes(bytes, Endian.little), 2147483648);
+    test('Monitor for distance data', () async {
+      final fakeData = Stream<List<int>>.fromIterable([
+        // Each StatusData1 is 18 bytes
+        [
+          0x31,
+          0,
+          0,
+          0,
+          30,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0
+        ], // 3 m
+        [
+          0x31,
+          0,
+          0,
+          0,
+          40,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0
+        ], // 4 m
+        [
+          0x31,
+          0,
+          0,
+          0,
+          50,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0
+        ], // 5 m
+      ]);
+      final erg = Ergometer(device, bleClient: mockBle);
+      final StreamSubscription connection =
+          erg.connectAndDiscover().listen((_) {});
+      Stream.value(ConnectionStateUpdate(
+              deviceId: device.id,
+              connectionState: DeviceConnectionState.connected,
+              failure: null))
+          .forEach(deviceConnectionController.add);
+      characteristicController.addStream(fakeData);
+      final result = expectLater(
+          erg.monitorForData({Keys.ELAPSED_DISTANCE_KEY}),
+          emitsInOrder([
+            Map.from({Keys.ELAPSED_DISTANCE_KEY: 3}),
+            Map.from({Keys.ELAPSED_DISTANCE_KEY: 4}),
+            Map.from({Keys.ELAPSED_DISTANCE_KEY: 5}),
+          ]));
+      await result;
+      await connection.cancel();
+    });
   });
 }
