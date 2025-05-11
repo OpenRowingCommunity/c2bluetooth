76ee32ba610ea55ee491d4787ad5b9cab5b7fb22
Bertrand Esperou <bertrand.esperou@gmail.com>
2025-05-11 07:13:08 +0000

    test dataplex

---
+++ test/src/dataplex_test.dart
@@ -0,0 +1,81 @@
+import 'dart:async';
+import 'dart:typed_data';
+
+import 'package:c2bluetooth/src/dataplex.dart';
+import 'package:c2bluetooth/src/packets/base.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockBle extends Mock implements FlutterReactiveBle {}
+
+class FakeDevice extends Fake implements DiscoveredDevice {
+  @override
+  String get id => '01:23:45:67:89:AB';
+}
+
+class MockParse extends Mock {
+  Concept2CharacteristicData? call(Uint8List bytes);
+}
+
+void main() {
+  setUpAll(() {
+    registerFallbackValue(QualifiedCharacteristic(
+      serviceId: Uuid.parse('00000000-0000-0000-0000-000000000000'),
+      characteristicId: Uuid.parse('ce060030-43e5-11e4-916c-0800200c9a66'),
+      deviceId: 'fallback',
+    ));
+    registerFallbackValue(Uint8List(0));
+  });
+
+  group('Dataplex', () {
+    late MockBle ble;
+    late DiscoveredDevice device;
+
+    setUp(() {
+      ble = MockBle();
+      device = FakeDevice();
+
+      when(() => ble.subscribeToCharacteristic(any()))
+          .thenAnswer((_) => const Stream.empty());
+    });
+
+    test('creates multiplex subscription on construction', () {
+      Dataplex(device, ble);
+
+      verifyNever(() => ble.subscribeToCharacteristic(
+            any(
+                that: isA<QualifiedCharacteristic>().having(
+              (c) => c.deviceId,
+              'device ID',
+              equals(device.id),
+            )),
+          ));
+    });
+    test('dispose cancels BLE subscriptions and closes outgoing streams',
+        () async {
+      final bleStream = StreamController<Uint8List>();
+      when(() => ble.subscribeToCharacteristic(any()))
+          .thenAnswer((_) => bleStream.stream);
+
+      final dataplex = Dataplex(device, ble);
+
+      final s1 = dataplex.createStream({'a'}.toSet()).listen((_) {});
+      final s2 = dataplex.createStream({'b'}.toSet()).listen((_) {});
+      verify(() => ble.subscribeToCharacteristic(
+            any(
+                that: isA<QualifiedCharacteristic>().having(
+              (c) => c.deviceId,
+              'device ID',
+              equals(device.id),
+            )),
+          )).called(1);
+      dataplex.dispose();
+
+      await expectLater(s1.asFuture<void>(), completes);
+      await expectLater(s2.asFuture<void>(), completes);
+
+      expect(() => bleStream.add(Uint8List.fromList([0x00])), returnsNormally);
+    });
+  });
+}
