ea03af1adf195e4407d5c1a37f3f5362d134d1e4
Bertrand Esperou <bertrand.esperou@gmail.com>
2025-06-03 20:02:56 +0000

    declaring back extensions tests

--- test/extensions_test.dart
+++ test/extensions_test.dart
@@ -7,16 +7,16 @@ void main() {
   group("Concept2DateExtension - ", () {
     test('converting a time from bytes', () {
       final bytes = Uint8List.fromList([156, 42, 1, 14]); //date 10908
-      expect(
-          Concept2DateExtension.fromBytes(bytes), DateTime(2021, 12, 9, 14, 1));
-      // expect(CsafeIntExtension.fromBytes(bytes, Endian.little), 2147483648);
+      final date = Concept2DateExtension.fromBytes(bytes);
+      expect(date, DateTime(2021, 12, 9, 14, 1));
+      expect(date.toBytes(), bytes);
     });
 
     test('converting another time from bytes', () {
       final bytes = Uint8List.fromList([241, 45, 1, 14]);
-      expect(
-          Concept2DateExtension.fromBytes(bytes), DateTime(2022, 1, 31, 14, 1));
-      // expect(CsafeIntExtension.fromBytes(bytes, Endian.little), 2147483648);
+      final date = Concept2DateExtension.fromBytes(bytes);
+      expect(date, DateTime(2022, 1, 31, 14, 1));
+      expect(date.toBytes(), bytes);
     });
 
     test('converting a date from a verifiable source from bytes', () {
@@ -25,16 +25,17 @@ void main() {
       // 00011101 10010100  00010000 00001001
       // final bytes = Uint8List.fromList([29, 148, 16, 9]);
       final bytes = Uint8List.fromList([148, 29, 9, 16]);
-
-      expect(Concept2DateExtension.fromBytes(bytes),
-          DateTime(2014, 4, 25, 16, 09));
+      final date = Concept2DateExtension.fromBytes(bytes);
+      expect(date, DateTime(2014, 4, 25, 16, 09));
       // 25th April 2014 @ 16:09 (4:09pm)
+      expect(date.toBytes(), bytes);
     });
 
     test('converting a date in 2022 from bytes', () {
       final bytes = Uint8List.fromList([133, 44, 14, 14]);
-      expect(
-          Concept2DateExtension.fromBytes(bytes), DateTime(2022, 5, 8, 14, 14));
+      final date = Concept2DateExtension.fromBytes(bytes);
+      expect(date, DateTime(2022, 5, 8, 14, 14));
+      expect(date.toBytes(), bytes);
     });
   });
 
